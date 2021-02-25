832d465afbbc0e05a145e76c1b5b6146514e61a2
Raisenil <you@example.com>
2021-02-25 11:06:43 +0600

    BFS DFS

---
+++ Day 4 class/Bfs.py
@@ -0,0 +1,41 @@
+graph = {
+    "Arad": {"Timisoara": 118, "Sibiu": 140, "Zerind": 75},
+    "Zerind": {"Arad": 75, "Oradea": 71},
+    "Oradea": {"Zerind": 71, "Sibiu": 151},
+    "Timisoara": {"Arad": 118, "Lugoj": 111},
+    "Lugoj": {"Timisoara": 111, "Mehadia": 70},
+    "Mehadia": {"Lugoj": 70, "Dobreta": 75},
+    "Dobreta": {"Mehadia": 75, "Craiova": 120},
+    "Craiova": {"Dobreta": 120, "RimnicuVilcea": 146, "Pitesi": 138},
+    "RimnicuVilcea": {"Craiova": 146, "Pitesi": 97, "Sibiu": 80},
+    "Sibiu": {"Arad": 140, "Oradea": 151, "RimnicuVilcea": 80, "Fagaras": 99},
+    "Fagaras": {"Sibiu": 99, "Bucharest": 211},
+    "Pitesi": {"Bucharest": 101, "RimnicuVilcea": 97, "Craiova": 138},
+    "Bucharest": {"Pitesi": 101, "Fagaras": 211, "Giurgiu": 90, "Urziceni": 85},
+    "Giurgiu": {"Bucharest": 90},
+    "Urziceni": {"Bucharest": 85, "Hirsova": 98, "Vaslui": 142},
+    "Hirsova": {"Urziceni": 98, "Eforie": 86},
+    "Eforie": {"Hirsova": 86},
+    "Vaslui": {"Urziceni": 142, "Iasi": 92},
+    "Iasi": {"Vaslui": 92, "Neamt": 87},
+    "Neamt": {"Iasi": 87}
+}
+visited = []
+queue = []
+
+
+def bfs(visited, graph, node):
+    visited.append(node)
+    queue.append(node)
+
+    while queue:
+        s = queue.pop(0)
+        print(s, end=" ")
+
+        for neighbour in graph[s]:
+            if neighbour not in visited:
+                visited.append(neighbour)
+                queue.append(neighbour)
+
+
+bfs(visited, graph, 'Arad')
